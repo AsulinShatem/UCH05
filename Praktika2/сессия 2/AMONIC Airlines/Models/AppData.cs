d75a49431b687eba7155570bea0983917193d27a
LAVROV <111111@gmail.com>
2023-04-24 12:29:19 +0300

    AMONIC

---
+++ Praktika2/сессия 2/AMONIC Airlines/Models/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMONIC_Airlines.Models
+{
+    public static class AppData
+    {
+        public static Session2_XXEntities db = new Session2_XXEntities();
+    }
+}
